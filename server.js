87c03ba916c5809f3de4f9095305723d2dfe8673
Janak Maloney <JanakMaloney16@gmail.com>
2018-02-11 20:26:21 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function (request, response){
+    console.log('client request URL: ', request.url);
+    if (request.url === '/') {
+        fs.readFile('index.html', 'utf8', function (errors, contents) {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(contents);
+            response.end();
+        });
+    } else if (request.url === "/dojo.html") {
+        fs.readFile('dojo.html', 'utf8', function (errors, contents) {
+            response.writeHead(200, { 'Content-type': 'text/html' });
+            response.write(contents);
+            response.end();
+        });
+    } else if (request.url === "/ninjas.html") {
+        fs.readFile('ninjas.html', 'utf8', function (errors, contents) {
+            response.writeHead(200, { 'Content-type': 'text/html' });
+            response.write(contents);
+            response.end();
+        });
+    } else {
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+
+});
+server.listen(6789);
+// print to terminal window
+console.log("Running in localhost at port 6789");
